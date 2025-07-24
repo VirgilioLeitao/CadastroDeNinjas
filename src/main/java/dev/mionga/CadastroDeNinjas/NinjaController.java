42db1c95f48ab931095ca5b522500d14cee44185
Virgilio Leitao <kingmionga@Mac.lan>
2025-07-24 15:23:38 +0100

    Cria uma entidade na base de dados apartir do NinjaModel

--- src/main/java/dev/mionga/CadastroDeNinjas/NinjaController.java
+++ src/main/java/dev/mionga/CadastroDeNinjas/NinjaController.java
@@ -1,12 +1,11 @@
 package dev.mionga.CadastroDeNinjas;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class NinjaController {
 
     @GetMapping("/boasvindas")
     public String boasVindas() {
