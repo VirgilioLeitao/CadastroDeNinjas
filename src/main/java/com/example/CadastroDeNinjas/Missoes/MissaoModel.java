43b103442e0508d020a5ad9115c4e2d85b5657a7
Virgílio Leitão <virgilio.17@hotmail.com>
2025-07-29 19:59:33 +0100

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/com/example/CadastroDeNinjas/Missoes/MissaoModel.java
+++ src/main/java/com/example/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -2,11 +2,17 @@ package com.example.CadastroDeNinjas.Missoes;
 
 import com.example.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table (name = "tb_missao")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissaoModel {
 
     @Id
@@ -19,32 +25,4 @@ public class MissaoModel {
     @OneToMany(mappedBy = "missao")
     private List<NinjaModel> ninja;
 
-
-
-/*
-    public MissaoModel(long id) {
-        this.id = id;
-    }
-
-    public MissaoModel(String nome, String dificuldade) {
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
- */
 }
--- src/main/java/com/example/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/example/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,13 +2,18 @@ package com.example.CadastroDeNinjas.Ninjas;
 
 import com.example.CadastroDeNinjas.Missoes.MissaoModel;
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //Entity transforma uma classe comum em uma entidade de BD
 //JPA = Java Persistence API
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class NinjaModel {
 
     @Id
@@ -24,38 +29,4 @@ public class NinjaModel {
     @JoinColumn(name = "missao_id") //Foreing Key ou chave estrangeira
     private MissaoModel missoes;
 
-
-    public NinjaModel(String name) {
-        this.name = name;
-    }
-
-    public NinjaModel(String name, String email, int idade) {
-        this.name = name;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
