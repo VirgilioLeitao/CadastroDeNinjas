c09ed29ff1409d0d25479b320c81d259c1b88187
Virgílio Leitão <virgilio.17@hotmail.com>
2025-08-12 16:58:40 +0100

    deat: Cria as rotas do controller com mock

--- src/main/java/com/example/CadastroDeNinjas/Missoes/MissaoController.java
+++ src/main/java/com/example/CadastroDeNinjas/Missoes/MissaoController.java
@@ -1,4 +1,27 @@
 package com.example.CadastroDeNinjas.Missoes;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("missoes")
 public class MissaoController {
+
+    @GetMapping("/lista")
+    public String listarMissao(){
+        return "Missao listada com sucesso";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missao criada com sucesso";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missao alterada com sucesso";
+    }
+
+    @PutMapping("/deletar")
+    public String deletarMissao(){
+        return "Missao deletada com sucesso";
+    }
 }
